0499d4f3eeb138e7f41f31099fb5ff9bc896508b
javawithlwl <javawithlwl@gmail.com>
2023-06-25 16:08:12 +0530

    Added hello world devops

---
+++ src/main/java/com/lwl/helloworlddevops/HelloWorldDevopsApplication.java
@@ -0,0 +1,13 @@
+package com.lwl.helloworlddevops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldDevopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldDevopsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lwl/helloworlddevops/HelloWorldDevopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lwl.helloworlddevops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldDevopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
